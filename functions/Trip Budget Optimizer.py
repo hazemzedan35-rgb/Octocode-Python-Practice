a3c98f4a25254ecbfa6dc8084ec14d6eb06bf72c
Moamen Hazem Abdelmoaty Zedan Saad <hazemzedan35@gmail.com>
2026-06-12 18:35:10 +0300

    Add files via upload

---
+++ functions/Trip Budget Optimizer.py
@@ -0,0 +1,10 @@
+# an application that calculate the the budget of travels and journeys
+def calculate_travel_expenses(days, cost):
+    return (days * cost) + 500
+total_cost = calculate_travel_expenses(int(input("number of days?"), input("food cost?")))
+def convert_currency():
+    return total_cost / 50
+
+
+
+
---
+++ functions/nome.py
@@ -0,0 +1,4 @@
+print ("welcome to youtube channel name generator\n")
+name = input("what is your name?\n")
+category = input ("what is your channel about\n")
+print("your channel can be named: ",category , " by ",name+str(len(name)))
