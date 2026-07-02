5204aa200b474c24f71b84a05ca2b429c33301e0
Moamen Hazem Abdelmoaty Zedan Saad <hazemzedan35@gmail.com>
2026-07-02 13:14:28 +0300

    Add files via upload

---
+++ functions/saying.py
@@ -0,0 +1,17 @@
+def main():
+    hello("moamen")
+    good_bye("moamen")
+
+def hello(name):
+    print(f"hello {name}")
+
+def good_bye(name):
+    print(f"good bye {name}")
+
+if __name__=="__main__":
+    main()
+
+
+
+
+    
