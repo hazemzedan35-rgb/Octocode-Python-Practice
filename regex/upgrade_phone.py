300112b14671d39a4ec8b996a75ce74aef033721
Moamen Hazem Abdelmoaty Zedan Saad <286130044+hazemzedan35-rgb@users.noreply.github.com>
2026-07-28 21:05:02 +0300

    Add files via upload

---
+++ regex/upgrade_phone.py
@@ -0,0 +1,12 @@
+import re
+
+phone_number = input("what is ur phone number? ").strip()
+
+if re.search(r"^\d{3}-\d{3}-\d{4}$", phone_number):
+    print("valid")
+
+elif re.search(r"^\(\d{3}\) \d{3}-\d{4}$", phone_number):
+    print("valid")
+
+else:
+    print("invalid")
