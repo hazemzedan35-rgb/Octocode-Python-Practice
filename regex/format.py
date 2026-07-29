2d568fb25319085a35177fc86f8d833ae0258549
Moamen Hazem Abdelmoaty Zedan Saad <286130044+hazemzedan35-rgb@users.noreply.github.com>
2026-07-29 15:22:01 +0300

    Add files via upload

---
+++ regex/format.py
@@ -0,0 +1,9 @@
+import re 
+
+name = input("what's your name? ").strip()
+matches = re.search(r"^(\w+),? ?(\w+)$", name)
+
+if matches:
+    
+    name = matches.group(2)+ " " + matches.group(1)
+    print(f"hello, {name}")
